c3c9e8b77c59d5d7055ee8305b9040015562562f
Irene Ockrah <ostrene33@gmail.com>
2022-09-11 06:00:29 -0700

    introducing react redux

---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,18 @@
+let initialState = {
+  contacts: [
+    { name: "Kwame Mens", phonenumber: "0243786321", location: "Accra", id:"1cd" },
+		{ name: "Abena John", phonenumber: "0509876310", location: "Koforidua", id:"2cd" },
+		{ name: "Kweku Sark", phonenumber: "0263337730", location: "Nalerigu", id:"3cd" },
+  ],
+};
+
+
+
+let contactReducer =(state=initialState, action)=>{
+	//WRITE IF ELSE LOGIC TO HANDLE action
+	//
+  return state
+
+}
+
+export default contactReducer;
