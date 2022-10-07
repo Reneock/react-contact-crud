aad9397efe26edfc76846587dbfd6e4e1d2da764
Irene Ockrah <ostrene33@gmail.com>
2022-10-07 04:41:33 -0700

    updating App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
 import Contacts from "./components/Contacts";
@@ -6,30 +6,16 @@ import ContactsForm from "./components/ContactsForm";
 import './App.css';
 
 function App() {
-	const [contacts, setContacts] = useState([]);
-
-	const addNewContact = (contact) => {
-		contact.id = Math.random().toString();
-		setContacts([...contacts, contact]);
-	};
-
-	const deleteContact = (id, deletedContact) => {
-		setContacts(contacts.filter(contact => contact.id === id ? deletedContact : contact))
-	};
-
-	const editContact = (id, updatedContact) => {
-		setContacts(contacts.map(contact => contact.id === id ? updatedContact : contact))
-	};
 
 	return (
     <>
 		  <Container fluid  style={{marginTop: "2rem"}}>
 			  <Row>
 				  <Col md="4">
-					  <ContactsForm newContact={addNewContact} />
+					  <ContactsForm />
 				  </Col>
 				  <Col>
-					  <Contacts contactInfo={contacts} deleteContact={deleteContact} editContact={editContact}/>
+					  <Contacts />
 				  </Col>
 			  </Row>
 		  </Container>
